a955f835369d5c5b9e068c1879083e0c162d6dda
numenization <nicholas.spencer5@gmail.com>
2023-05-23 15:56:25 -0400

    moved navbar to appjs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,12 @@
 import '@/css/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import MainNav from '@/components/navbar';
+
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <MainNav />
       <Component {...pageProps} />
     </div>
   );
